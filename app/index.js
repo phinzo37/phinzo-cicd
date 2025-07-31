42d7e3f089d6997669d15e0174da1effb7c86721
Anil Gurung <anilgurung@anils-mbp.mynetworksettings.com>
2025-07-31 13:22:33 -0400

    test

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Hello from Phinzo DevOps Project!"));
+app.get("/", (req, res) => res.send("This is test"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
